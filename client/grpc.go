49f7f1c13cabc2f347970b5a4fd3bd2721ddd57a
kehiy <kehiiiiya@gmail.com>
2023-07-12 00:56:15 +0800

    fix: grpc client

---
+++ client/grpc.go
@@ -0,0 +1,68 @@
+package client
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/k0kubun/pp"
+	"github.com/pactus-project/pactus/crypto"
+	"github.com/pactus-project/pactus/crypto/bls"
+	pactus "github.com/pactus-project/pactus/www/grpc/gen/go"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	networkClient     pactus.NetworkClient
+	conn              *grpc.ClientConn
+}
+
+func NewClient(endpoint string) (*Client, error) {
+	conn, err := grpc.Dial(endpoint,
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	pp.Println("connection established...")
+
+	return &Client{
+		networkClient:     pactus.NewNetworkClient(conn),
+		conn:              conn,
+	}, nil
+
+}
+
+func (c *Client) GetNetworkInfo() (*pactus.GetNetworkInfoResponse, error) {
+	networkInfo, err := c.networkClient.GetNetworkInfo(context.Background(), &pactus.GetNetworkInfoRequest{})
+	if err != nil {
+		log.Printf("error obtaining network information: %v", err)
+
+		return nil, err
+	}
+
+	return networkInfo, nil
+}
+
+func (c *Client) GetPeerInfo(address string) (*pactus.PeerInfo, *bls.PublicKey, error) {
+	networkInfo, _ := c.GetNetworkInfo()
+	crypto.PublicKeyHRP = "tpublic"
+	if networkInfo != nil {
+		for _, p := range networkInfo.Peers {
+			for _, key := range p.ConsensusKeys {
+				pub, _ := bls.PublicKeyFromString(key)
+				if pub != nil {
+					if pub.Address().String() == address {
+						return p, pub, nil
+					}
+				}
+			}
+		}
+	}
+	return nil, nil, errors.New("peer does not exist")
+}
+
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,17 @@ go 1.20
 require github.com/xuri/excelize/v2 v2.7.1
 
 require (
+	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0 // indirect
+	github.com/kilic/bls12-381 v0.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+)
+
+require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pactus-project/pactus v0.13.0 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
